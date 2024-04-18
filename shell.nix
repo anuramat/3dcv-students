2520710809c70f7502f814c91d8e51056097f7ae
Arsen Nuramatov <anuramat@pm.me>
2024-04-18 22:27:32 +0500

    feat: add nix shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python3.withPackages (python-pkgs: [
+        python-pkgs.numpy
+        python-pkgs.matplotlib
+        python-pkgs.scikit-image
+        python-pkgs.jupyter
+        # todo jupyter
+      ]))
+    ];
+  }
